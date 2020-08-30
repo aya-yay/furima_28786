7cb5e1b29b8d74b8a478e0db7d63361166b01c92
aya-yay <69228983+aya-yay@users.noreply.github.com>
2020-08-30 17:01:47 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, except: [:index, :show]
  
   def index
@@ -35,11 +35,10 @@ class ItemsController < ApplicationController
   end
 
 
-  # def destroy
-  #   item = Item.find(params[:id])
-  #   item.destroy
-  #   redirect_to root_path
-  # end
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
 
   private
 
