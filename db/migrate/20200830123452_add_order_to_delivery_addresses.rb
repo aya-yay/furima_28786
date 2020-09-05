43e6f6c2a047025101eaea0591d75c8da9251ae0
aya-yay <69228983+aya-yay@users.noreply.github.com>
2020-09-05 15:16:29 +0900

    deiete migtare

--- db/migrate/20200830123452_add_order_to_delivery_addresses.rb
+++ db/migrate/20200830123452_add_order_to_delivery_addresses.rb
@@ -1,5 +1,5 @@
 class AddOrderToDeliveryAddresses < ActiveRecord::Migration[6.0]
   def change
-    add_reference :delivery_addresses, :order, null: false, foreign_key: true
+    add_reference :delivery_addresses, null: false, foreign_key: true
   end
 end
